093c8911f9e287ba045015d2b67a510a804de457
ephrim-s <ephrims5@gmial.com>
2025-07-20 21:02:22 +0000

    first commit

---
+++ alx-project-0x01/pages/index.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import React from "react";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const Home: React.FC = () => {
+  return ( 
+
+    <div className="flex justify-center items-center h-screen">
+
+      <h1 className="text-7xl font-thin">Welcome Page</h1>
+
+    </div>
+
+  );
+}
+export default Home;
